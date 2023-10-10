aeba9c3c40d1b202d3c62a1c1d96e09b08c7050f
katenester1995 <nesterenkokm@bk.ru>
2023-10-10 23:02:22 +0300

    First processed product

---
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,62 @@
+﻿namespace ConsoleApp
+{
+    enum Day
+    {
+        morning,
+        afternoon,
+        evening,
+    }
+    internal class Program
+    {
+        // Авторизация.
+        // P.S. тут будет проверка есть ли пользователь в системе , регистрация нового пользователя, авторизация предыдущего
+        public static string CheckUserName()
+        {
+            Console.Write("Введите игровое имя: ");
+            string UserName = Console.ReadLine();
+            // дописать проверки + что-то сделать с not null - метод isnullorempty 
+            return UserName;
+        }
+
+        // Предлагаю проверку делать тут, чтобы сделать код менее грамоздким и избежать повторения кода
+        // (Чапкин презирает повторения кода и буллит за это)
+        public static int TryInt()
+        {
+            // мб следует сделать через try catch ( надо будет перечитать код стайл)
+            while (true)
+            {
+                Console.WriteLine("Введите число:");
+                if (int.TryParse(Console.ReadLine(), out var value)) { return value; } 
+                else { Console.WriteLine("Некорректный формат введенных данных."); }  
+            }
+        }
+        static void Main()
+        {
+            // Получение текущего времени
+            DateTime dateTime = new DateTime();
+            // Получение текущего часа
+            int hour=dateTime.Hour;
+            if (hour > 5 && hour < 12)
+            {
+                Console.WriteLine($"Good {Day.morning}");
+            }
+            else if (hour >= 12 && hour < 19)
+            {
+                Console.WriteLine($"Good {Day.afternoon}");
+            }    
+            else
+            {
+                Console.WriteLine($"Good {Day.evening}");
+            }
+            string UserName= CheckUserName(); // авторизация / регистрация имени произошла успешно.
+            Console.WriteLine("Выберите из списка номер:");
+            Console.WriteLine("1. Загрузка предыдущей игры");
+            Console.WriteLine("2. Создание новой игры");
+            int x = TryInt();
+
+
+
+
+        }
+    }
+}
