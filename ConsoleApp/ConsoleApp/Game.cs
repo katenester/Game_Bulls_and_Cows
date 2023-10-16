34cb2cf470feae4e8d478e0c742a3e9775666299
katenester1995 <nesterenkokm@bk.ru>
2023-10-16 23:29:11 +0300

    Удаление лишних методов

--- ConsoleApp/ConsoleApp/Game.cs
+++ ConsoleApp/ConsoleApp/Game.cs
@@ -9,29 +9,6 @@ namespace ConsoleApp
         {
             
         }
-        // метод для новой игры
-        public static void NewPlay(string userName)
-        {
-            
-        }
-        // метод для создания числа ( здесь компьютер загадывает число). userName нужен для занесения в бд при сохранении незавершенной игры
-        public static int HiddenNumber(string userName)
-        {
-            return 0;
-        }
-        // Метод, возвращающий true - если пользователь найден , false - если отсутствует
-        public static bool Check(string UserName)
-        {
-            //ТУТ ЛОГИКА ТОЖЕ СЛОМАЛАСЬ 
-            //if (Data.CheckUser(UserName)) { return true; }
-            //else
-            return false;
-        }
-        /*/ метод инициализации старого пользователя
-        public static void Init(string userName)
-        {
-            
-        }
         // метод создания нового пользвоателя в бд*/
         // метод авторизации/ регистрации пользователя 
         public static void User(ref Info info)
